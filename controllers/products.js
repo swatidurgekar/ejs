63db32b5c6cef2c4fae120320fa86c61bf89fe96
swatidurgekar <swatidurgekar@gmail.com>
2023-08-21 20:45:25 +0530

    Controllers

---
+++ controllers/products.js
@@ -0,0 +1,34 @@
+const products = [];
+
+exports.getAddProduct = (req, res, next) => {
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect("/");
+};
+
+exports.getProducts = (req, res, next) => {
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
+};
+
+exports.pageNotFound = (req, res, next) => {
+  res.render("404", {
+    pageTitle: "Page Not Found",
+    path: "",
+  });
+};
